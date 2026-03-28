aba13527495555e6b0971058537068ef3e565bea
Kiyashan <airlinkride@gmail.com>
2026-03-28 15:21:06 -0400

    changed woodbridge sedan and suv prices

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -245,7 +245,7 @@ export default function Rates() {
         { location: "Whitby", sedan: 120, suv: 140 },
         { location: "Whitby North", sedan: 130, suv: 150},
         { location: "Windsor", sedan: 670, suv: 730 },
-        { location: "Woodbridge", sedan: 50, suv: 60 },
+        { location: "Woodbridge", sedan: 60, suv: 80 },
         { location: "Woodstock", sedan: 230, suv: 250 },
         { location: "Woodbine Garden", sedan: 82, suv: 99 },
         { location: "Woodbine Heights", sedan: 82, suv: 99 },
