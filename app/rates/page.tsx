fd5827c5dbb527985c8576ee599bf6568315eece
Kiyashan <airlinkride@gmail.com>
2026-04-27 21:22:52 -0400

    changed sedan suv price hamilton MTN

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -95,7 +95,7 @@ export default function Rates() {
       title: "Category H",
       rates: [
         { location: "Hamilton", sedan: 120, suv: 130},
-        { location: "Hamilton MTN", sedan: 150, suv: 165},
+        { location: "Hamilton MTN", sedan: 140, suv: 150},
         { location: "Hayden", sedan: 197, suv: 219 },
         { location: "Huntsville", sedan: 425, suv: 465 },
       ],
