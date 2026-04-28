101c0714aab05a866bf4cc069c31000977c4b459
Kiyashan <airlinkride@gmail.com>
2026-04-27 21:44:45 -0400

    changed sedan and suv price for stoney creek

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -203,7 +203,7 @@ export default function Rates() {
       rates: [
         { location: "Schomberg", sedan: 90, suv: 100 },
         { location: "St Catharines", sedan: 190, suv: 210 },
-        { location: "Stoney Creek", sedan: 135, suv: 155 },
+        { location: "Stoney Creek", sedan: 130, suv: 140 },
         { location: "Stouffville", sedan: 110, suv: 130 },
         { location: "Stratford", sedan: 245, suv: 265 },
         { location: "Scarborough", sedan: 80, suv: 90 },
