43e0ebdeb25f127ef032ac95cba17aec9bf01ad8
Kiyashan <airlinkride@gmail.com>
2026-05-05 18:07:48 -0400

    changed ashburn suv price

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -14,7 +14,7 @@ export default function Rates() {
         { location: "Alliston", sedan: 140, suv: 160 },
         { location: "Ancaster", sedan: 125, suv: 145 },
         { location: "Angus", sedan: 175, suv: 195 },
-        { location: "Ashburn", sedan: 130, suv: 150 },
+        { location: "Ashburn", sedan: 130, suv: 145 },
         { location: "Aurora", sedan: 100, suv: 110 },
       ],
     },
