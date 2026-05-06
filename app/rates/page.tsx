16f5c1aedcbd857834d4c36ded19824a799f378f
Kiyashan <airlinkride@gmail.com>
2026-05-05 21:17:10 -0400

    changed suv and sedan price for kilbride

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -115,7 +115,7 @@ export default function Rates() {
       
         rates: [
         { location: "Keswick", sedan: 160, suv: 170 },
-        { location: "Kilbride", sedan: 109, suv: 120 },
+        { location: "Kilbride", sedan: 105, suv: 115 },
         { location: "King City", sedan: 80, suv: 99 },
         { location: "Kingston", sedan: 440, suv: 470 },
         { location: "Kitchener", sedan: 170, suv: 190 },
