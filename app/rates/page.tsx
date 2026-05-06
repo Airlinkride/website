afe9c65480e9c824fea3cc69fb2f119aa0874475
Kiyashan <airlinkride@gmail.com>
2026-05-05 21:19:25 -0400

    changed sedan price for london

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -128,7 +128,7 @@ export default function Rates() {
       title: "Category L",
       rates: [
         { location: "Leaside Area", sedan: 81, suv: 100 },
-        { location: "London", sedan: 340, suv: 350 },
+        { location: "London", sedan: 335, suv: 350 },
         { location: "Loyalist", sedan: 520, suv: 560 },
         { location: "Linsday", sedan: 270, suv: 300 },
         { location: "Lincoln", sedan: 160, suv: 180 },
