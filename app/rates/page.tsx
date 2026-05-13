4e028134e37b695f53edaa805b5bfab6b753705b
Kiyashan <airlinkride@gmail.com>
2026-05-12 21:17:54 -0400

    changed sedan price for linsday

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -130,7 +130,7 @@ export default function Rates() {
         { location: "Leaside Area", sedan: 81, suv: 100 },
         { location: "London", sedan: 330, suv: 350 },
         { location: "Loyalist", sedan: 520, suv: 560 },
-        { location: "Linsday", sedan: 270, suv: 290 },
+        { location: "Linsday", sedan: 265, suv: 290 },
         { location: "Lincoln", sedan: 160, suv: 180 },
       ],
     },
