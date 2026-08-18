c955bc97fa4d51d541c6fe7315396210e83f4ca3
Kiyashan <airlinkride@gmail.com>
2026-08-17 22:58:48 -0400

    changed oshawa suv price

--- app/rates/RatesClient.tsx
+++ app/rates/RatesClient.tsx
@@ -167,7 +167,7 @@ export default function Rates() {
         { location: "Orangeville", sedan: 110, suv: 130 },
         { location: "Orillia", sedan: 230, suv: 260 },
         { location: "Orono", sedan: 180, suv: 200 },
-        { location: "Oshawa", sedan: 120, suv: 130 },
+        { location: "Oshawa", sedan: 120, suv: 140 },
         { location: "North Oshawa", sedan: 130, suv: 140},
         { location: "Ottawa", sedan: 850, suv: 900 },
         { location: "Owen Sound", sedan: 340, suv: 380 },
